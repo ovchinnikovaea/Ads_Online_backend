f790e9cb730abdf40555712bc331ecb5d200a0aa
ovchinnikova <ov4innikova.e.a@mail.ru>
2024-06-01 21:32:34 +0300

    Added annotations in dto for comments

--- src/main/java/ru/skypro/homework/dto/comments/CommentsDTO.java
+++ src/main/java/ru/skypro/homework/dto/comments/CommentsDTO.java
@@ -1,12 +1,15 @@
 package ru.skypro.homework.dto.comments;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import javax.validation.Valid;
 import java.util.List;
 @Data
 public class CommentsDTO {
-
+    @JsonProperty("count")
     private Integer count;
-
+    @JsonProperty("results")
+    @Valid
     private List<CommentDTO> results;
 }
