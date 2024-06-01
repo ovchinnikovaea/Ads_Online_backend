25dfa82a3fd7bec9b0a2435d50fb7a048b2902aa
Answer <a.n.s.w.e.r130597@gmail.com>
2024-06-01 19:57:38 +0700

    Добавлены DTO для объявлений

---
+++ src/main/java/ru/skypro/homework/dto/ads/CreateOrUpdateDto.java
@@ -0,0 +1,21 @@
+package ru.skypro.homework.dto.ads;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+@Data
+public class CreateOrUpdateDto {
+    @JsonProperty("description") // описание объявления
+    private String description;
+
+    @JsonProperty("title") // заголовок объявления
+    private String title;
+
+    @JsonProperty("price") // цена объявления
+    @Min(0)
+    @Max(10000000)
+    private Integer price;
+}
---
+++ src/main/java/ru/skypro/homework/dto/ads/ExtendedAdDto.java
@@ -0,0 +1,34 @@
+package ru.skypro.homework.dto.ads;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class ExtendedAdDto {
+    @JsonProperty("pk") // id объявления
+    private Integer pk;
+
+    @JsonProperty("authorFirstName") // имя автора объявления
+    private String authorFirstName;
+
+    @JsonProperty("authorLastName") // фамилия автора объявления
+    private String authorLastName;
+
+    @JsonProperty("description") // описание объявления
+    private String description;
+
+    @JsonProperty("email") // email автора объявления
+    private String email;
+
+    @JsonProperty("image") // ссылка на картинку объявления
+    private String image;
+
+    @JsonProperty("phone") // телефон автора объявления
+    private String phone;
+
+    @JsonProperty("price") // цена объявления
+    private Integer price;
+
+    @JsonProperty("title") // заголовок объявления
+    private String title;
+}
