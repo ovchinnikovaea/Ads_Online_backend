ea2895105125d4e96f3ba33343b7ebc80fa895af
Timofey <timofey.sharipov@gmail.com>
2024-06-11 01:46:12 +0300

    add LoginEntity, RegisterMapper, LoginRepository

--- src/main/java/ru/skypro/homework/mapper/RegisterMapper.java
+++ src/main/java/ru/skypro/homework/mapper/RegisterMapper.java
@@ -1,12 +1,14 @@
 package ru.skypro.homework.mapper;
 /*маппер для регистрациию*/
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.skypro.homework.dto.autoAndReg.Register;
+import ru.skypro.homework.model.User;
 
-@Component
-public class RegisterMapper {
-    public Register mapstruct () {
-        Register register = new Register();
-        return Register;
-    }
+@Mapper(componentModel = "spring")
+public abstract class RegisterMapper {
+    public abstract Register toDto(User user);
+
+    @Mapping(target = "email", source = "username")
+    public abstract User toEntity(Register userDto);
 }
