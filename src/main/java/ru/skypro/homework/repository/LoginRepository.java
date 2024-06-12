58c57c3108d4b2c6dfa0d61717e01bca3de6dfa8
Timofey <timofey.sharipov@gmail.com>
2024-06-12 15:14:34 +0300

    rename Authorities to LoginEntity

--- src/main/java/ru/skypro/homework/repository/LoginRepository.java
+++ src/main/java/ru/skypro/homework/repository/LoginRepository.java
@@ -2,11 +2,11 @@ package ru.skypro.homework.repository;
 /*Репозиторий авторизация*/
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.skypro.homework.entity.LoginEntity;
+import ru.skypro.homework.entity.Authorities;
 
 import java.util.Optional;
 
 @Repository
-public interface LoginRepository extends JpaRepository<LoginEntity, Integer> {
-    Optional<LoginEntity> findByUsername(String username);
+public interface LoginRepository extends JpaRepository<Authorities, Integer> {
+    Optional<Authorities> findByUsername(String username);
 }
