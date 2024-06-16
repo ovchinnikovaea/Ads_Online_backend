358e3877d57da4f7e135545edd040092db11df1f
ovchinnikova <ov4innikova.e.a@mail.ru>
2024-06-16 21:09:00 +0300

    Added CommentService

--- src/main/java/ru/skypro/homework/repository/CommentRepository.java
+++ src/main/java/ru/skypro/homework/repository/CommentRepository.java
@@ -3,5 +3,12 @@ package ru.skypro.homework.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.skypro.homework.entity.Comment;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+    List<Comment> findByAdId(int id);
+
+    Comment findByAdIdAndId(int adId, int CommentId);
+    void deleteByAdIdAndId(int adId, int CommentId);
+
 }
