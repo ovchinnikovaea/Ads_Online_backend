90e38887c1549676c0b8eba79a49e8591a6e9568
Answer <a.n.s.w.e.r130597@gmail.com>
2024-06-20 00:48:54 +0700

    Добавление UserController

--- src/main/java/ru/skypro/homework/controller/UserController.java
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -1,43 +1,41 @@
 package ru.skypro.homework.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.user.NewPasswordDto;
 import ru.skypro.homework.dto.user.UpdateUserDto;
-import ru.skypro.homework.dto.user.UserDto;
-import ru.skypro.homework.mapper.UserMapper;
-import ru.skypro.homework.repository.UserRepository;
 import ru.skypro.homework.service.UserService;
+import org.springframework.http.ResponseEntity;
 
-import java.awt.*;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private UserRepository repository;
     @Autowired
     private UserService userService;
 
-    private UserMapper mapper;
+    @PostMapping(value = "/users/set_password")
+    public ResponseEntity<?> setPassword(@Valid @RequestBody NewPasswordDto body, Authentication authentication) {
+        userService.updateNewPassword(body, authentication);
+        return ResponseEntity.ok().build();
+    }
 
-    public UserController(UserRepository repository, UserMapper mapper) {
-        this.repository = repository;
-        this.mapper = mapper;
+    @PatchMapping(value = "/users/me")
+    public ResponseEntity<UpdateUserDto> UpdateUserDto(@Valid @RequestBody UpdateUserDto body, Authentication authentication) {
+        return ResponseEntity.ok(userService.updateUser(body, authentication));
     }
-    @PostMapping
-    public NewPasswordDto set_password(){
-        return null;
+
+    @PatchMapping(value = "/users/me/image")
+    public ResponseEntity<Void> updateUserImage(@Valid @RequestPart(value = "image", required = false) MultipartFile image, Authentication authentication) {
+        userService.uploadImage(image, authentication);
+        return ResponseEntity.ok().build();
     }
-    @GetMapping
-    public UserDto me(){
-        return null;
-    }
-//    @PatchMapping
-//    private UpdateUserDto me(){
-//        return null;
-//    }
-    @PatchMapping("/me")
-    private Image image(){
-        return null;
+
+    @GetMapping(value = "/users/{id}/image")
+    public ResponseEntity<byte[]> getUserImage(@PathVariable int id) {
+        return ResponseEntity.ok(userService.getUserImage(id));
     }
 }
